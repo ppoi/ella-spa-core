49fa453ee703024d3f0205ed1795db11141be15e
satoshi.takano.yj <satoshi.takano.yj@hitachi-solutions.com>
2024-12-17 04:30:16 +0000

    wip: blocled navigation

--- widget/navigation/navigation-item.svelte
+++ widget/navigation/navigation-item.svelte
@@ -7,12 +7,23 @@
    * @property {boolean} [active] - アクティブ状態
    * @property {Function} [onclick] - clickイベントハンドラ
    * @property {Function} [onmouseenter] - mouseenterイベントハンドラ
+   * @property {boolean} [blocked] - 閉塞フラグ
    */
   /** @type Props */
-  let { path, icon, label, active = $bindable(false), onclick, onmouseenter } = $props();
+  let { path, icon, label, active = $bindable(false), blocked, onclick, onmouseenter } = $props();
+
+  function eventHandler(callback) {
+    return (e)=>{
+      if(blocked) {
+        e.preventDefault();
+      } else {
+        callback?.();
+      }
+    }
+  }
 </script>
 
-<a href="{path}" class="section-link" class:active="{ active }" {onclick} {onmouseenter}>
+<a href="{path}" class="section-link" class:active="{ active }" class:blocked={blocked} onclick={eventHandler(onclick)} onmouseover={eventHandler(onmouseenter)}>
   <span class="navicon material-symbols-outlined">{ icon }</span>
   <div class="label">{ label }</div>
 </a>
